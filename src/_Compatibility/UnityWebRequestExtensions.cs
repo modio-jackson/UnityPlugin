ffbbfb0ce61ab625bc8b0bf17edc956bcffaa086
Jackson Wood <jackson@melodatron.com>
2019-10-29 11:26:00 +1100

    Implemented SendWebRequest as a UnityWebRequestExtension

---
+++ src/_Compatibility/UnityWebRequestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Networking;
+
+namespace ModIO
+{
+    #if !UNITY_2017_2_OR_NEWER
+    public static class UnityWebRequestAsyncOperationExtensions
+    {
+        public static ModIO.Compatibility.UnityWebRequestAsyncOperation SendWebRequest(this UnityWebRequest request)
+        {
+            UnityEngine.AsyncOperation operation = request.Send();
+
+            var operationWrapper = new ModIO.Compatibility.UnityWebRequestAsyncOperation(request, operation);
+            return operationWrapper;
+        }
+    }
+    #endif
+}
